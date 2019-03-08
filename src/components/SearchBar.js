39983278d28961ba4cce004456e3c648a269f204
hungmuha <hung.soboss@gmail.com>
2019-03-08 14:36:03 -0700

    Added some more code to get ready for Ajax call to Upsplash API

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,32 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    //naming convention, "on or handle" "name of the el" "action"
+    // onInputChange(event){
+    //     console.log(event.target.value);
+    // }
+
+    state = { term: ""};
+
+    onFormSubmit = event => {
+        event.preventDefault();
+        
+        this.props.onSubmit(this.state.term);
+    }
+
     render(){
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit = {this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text"/>
+                        {/* by leaving off the () the function only called in the future 
+                         with exception of using => function, it will only invoke in the future*/}
+                        <input 
+                            type="text" 
+                            value = {this.state.term} 
+                            onChange={(e)=> this.setState({term: e.target.value})}
+                        />
                     </div>
                 </form>
             </div>
